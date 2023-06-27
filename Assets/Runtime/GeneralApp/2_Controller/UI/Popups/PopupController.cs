d841f52f47a6ac574cb26e9f75d372410bb02af0
YannickSCF <yannickscf@gmail.com>
2023-06-27 13:47:09 +0200

    Added view abstract methods on controller

--- Assets/Runtime/GeneralApp/2_Controller/UI/Popups/PopupController.cs
+++ Assets/Runtime/GeneralApp/2_Controller/UI/Popups/PopupController.cs
@@ -32,24 +32,34 @@ namespace YannickSCF.GeneralApp.Controller.UI.Popups {
 
         public virtual void Init(string popupId) {
             _popupId = popupId;
+
+            View.Init();
         }
 
         public virtual void Open() {
             gameObject.SetActive(true);
+
+            View.Open();
         }
 
         public virtual void Show(Action<PopupController, string> onPopupShown = null) {
             OnPopupShown = onPopupShown;
             gameObject.SetActive(true);
+
+            View.Show();
         }
 
         public virtual void Hide(Action<PopupController, string> onPopupHidden = null) {
             OnPopupHidden = onPopupHidden;
             gameObject.SetActive(false);
+
+            View.Hide();
         }
 
         public virtual void Close(Action onPopupClosed = null) {
             Destroy(gameObject);
+
+            View.Close();
         }
     }
 }
