0ae39cf63f8c06f4295d9014f633548ad7bb4878
YannickSCF <yannickscf@gmail.com>
2023-10-24 11:57:26 +0200

    Added features to multi graphic input field

--- Assets/Runtime/GeneralApp/3_View/Components Tools/InputComponents/MultiGraphicTMPInputField.cs
+++ Assets/Runtime/GeneralApp/3_View/Components Tools/InputComponents/MultiGraphicTMPInputField.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -14,33 +15,42 @@ namespace YannickSCF.GeneralApp.View.ComponentTools.InputComponents {
 
         public bool UseTargetGraphic = false;
         public bool UseTextComponent = false;
+        public ColorBlock TextComponentColors;
         public bool UsePlaceholder = false;
+        public ColorBlock PlaceholderColors;
 
         protected override void DoStateTransition(SelectionState state, bool instant) {
-            var targetColor =
-                state == SelectionState.Disabled ? colors.disabledColor :
-                state == SelectionState.Highlighted ? colors.highlightedColor :
-                state == SelectionState.Normal ? colors.normalColor :
-                state == SelectionState.Pressed ? colors.pressedColor :
-                state == SelectionState.Selected ? colors.selectedColor : Color.white;
+            Color targetColor;
 
             if (UseAllGraphicChilds) {
+                targetColor = GetColorFromBlock(state, colors);
                 foreach (var graphic in GetComponentsInChildren<Graphic>()) {
                     graphic.CrossFadeColor(targetColor, instant ? 0f : colors.fadeDuration, true, true);
                 }
             } else {
                 if (UseTargetGraphic) {
+                    targetColor = GetColorFromBlock(state, colors);
                     image.GetComponent<Graphic>().CrossFadeColor(targetColor, instant ? 0f : colors.fadeDuration, true, true);
                 }
 
                 if (UseTextComponent) {
+                    targetColor = GetColorFromBlock(state, TextComponentColors);
                     textComponent.GetComponent<Graphic>().CrossFadeColor(targetColor, instant ? 0f : colors.fadeDuration, true, true);
                 }
 
                 if (UsePlaceholder) {
+                    targetColor = GetColorFromBlock(state, PlaceholderColors);
                     placeholder.GetComponent<Graphic>().CrossFadeColor(targetColor, instant ? 0f : colors.fadeDuration, true, true);
                 }
             }
+        }
+
+        private Color GetColorFromBlock(SelectionState state, ColorBlock colorsBlock) {
+            return state == SelectionState.Disabled ? colorsBlock.disabledColor :
+                state == SelectionState.Highlighted ? colorsBlock.highlightedColor :
+                state == SelectionState.Normal ? colorsBlock.normalColor :
+                state == SelectionState.Pressed ? colorsBlock.pressedColor :
+                state == SelectionState.Selected ? colors.selectedColor : Color.white;
         }
     }
 
@@ -56,19 +66,36 @@ namespace YannickSCF.GeneralApp.View.ComponentTools.InputComponents {
                 EditorGUILayout.LabelField("Graphics to use on Color Tint", EditorStyles.boldLabel);
                 EditorGUI.indentLevel++;
 
-                targetInputField.UseAllGraphicChilds = EditorGUILayout.Toggle("All Graphics", targetInputField.UseAllGraphicChilds);
+                GUIContent useAllGraphicChildsGUI = new GUIContent("All Graphics", "Using default colors of this component");
+                targetInputField.UseAllGraphicChilds = EditorGUILayout.ToggleLeft(useAllGraphicChildsGUI, targetInputField.UseAllGraphicChilds);
 
                 if (!targetInputField.UseAllGraphicChilds) {
                     EditorGUI.indentLevel++;
-                    targetInputField.UseTargetGraphic = EditorGUILayout.Toggle("Target Graphic", targetInputField.UseTargetGraphic);
-                    targetInputField.UseTextComponent = EditorGUILayout.Toggle("Text Component", targetInputField.UseTextComponent);
-                    targetInputField.UsePlaceholder = EditorGUILayout.Toggle("Placeholder", targetInputField.UsePlaceholder);
+                    targetInputField.UseTargetGraphic = EditorGUILayout.ToggleLeft("Target Graphic", targetInputField.UseTargetGraphic);
+                    targetInputField.TextComponentColors = PaintColorBlockSection(targetInputField.TextComponentColors, "Text Component", ref targetInputField.UseTextComponent);
+                    targetInputField.PlaceholderColors = PaintColorBlockSection(targetInputField.PlaceholderColors, "Placeholder", ref targetInputField.UsePlaceholder);
                     EditorGUI.indentLevel--;
                 }
 
                 EditorGUI.indentLevel--;
                 EditorGUILayout.Space();
             }
+        }
+
+        private ColorBlock PaintColorBlockSection(ColorBlock colorBlock, string title, ref bool conditionalShow) {
+            conditionalShow = EditorGUILayout.BeginToggleGroup(title, conditionalShow);
+            if (conditionalShow) {
+                EditorGUI.indentLevel++;
+                colorBlock.normalColor = EditorGUILayout.ColorField("Normal Color", colorBlock.normalColor, null);
+                colorBlock.highlightedColor = EditorGUILayout.ColorField("Highlighted Color", colorBlock.highlightedColor, null);
+                colorBlock.pressedColor = EditorGUILayout.ColorField("Pressed Color", colorBlock.pressedColor, null);
+                colorBlock.selectedColor = EditorGUILayout.ColorField("Selected Color", colorBlock.selectedColor, null);
+                colorBlock.disabledColor = EditorGUILayout.ColorField("Disabled Color", colorBlock.disabledColor, null);
+                EditorGUI.indentLevel--;
+            }
+            EditorGUILayout.EndToggleGroup();
+
+            return colorBlock;
         }
     }
 #endif
